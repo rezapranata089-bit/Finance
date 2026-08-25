d0421240591fca8d531a8fd300238728e5c651c8
rezapranata089-bit <rezapranata089@gmail.com>
2026-08-25 09:17:16 +0000

    Perbesar zoom Lottie lebih lanjut
    Sess: d834bd8029b29c0e

--- lib/main.dart
+++ lib/main.dart
@@ -824,7 +824,7 @@ class HomePage extends ConsumerWidget {
                                   height: 100,
                                   child: ClipRect(
                                     child: Transform.scale(
-                                      scale: 2.4,
+                                      scale: 4.0,
                                       child: Lottie.asset('assets/lottie/ai.json', repeat: true, fit: BoxFit.contain),
                                     ),
                                   ),
