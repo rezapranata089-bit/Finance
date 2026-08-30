17121f2f40a5ddd6ad906c5b9ccf7b7e18d07baf
rezapranata089-bit <rezapranata089@gmail.com>
2026-08-30 13:36:22 +0000

    Buat tint warna aksen scaffold lebih tipis
    Sess: d834bd8029b29c0e

--- lib/main.dart
+++ lib/main.dart
@@ -1569,7 +1569,7 @@ class MyFinanceApp extends ConsumerWidget {
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
-        scaffoldBackgroundColor: Color.alphaBlend(theme.primary.withOpacity(0.08), Colors.white),
+        scaffoldBackgroundColor: Color.alphaBlend(theme.primary.withOpacity(0.03), Colors.white),
         colorScheme: ColorScheme.fromSeed(
           seedColor: theme.primary,
           primary: theme.primary,
